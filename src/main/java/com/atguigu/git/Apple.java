59543dbfc2808e5cbf0c5d363b0f3c70d82049ce
eclipse_user <eclipse_user@atguigu.com>
2019-03-21 15:02:19 +0800

    My first commit in Eclipse

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * 打开Navigator 窗口 选中需要忽略的文件 右键 Team ignore
+ * @author ASUS-
+ *
+ */
+
+/**
+ * Team and to index 添加到暂存区的操作
+ * @author ASUS-
+ *
+ */
+public class Apple {
+
+}
